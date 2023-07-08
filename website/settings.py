fb5b9a244ac9f68d6e9d1ebbc8c9c826c974e0c9
Tom Stout <tom.stout325@gmail.com>
2023-07-07 22:07:13 -0400

    updates

--- website/settings.py
+++ website/settings.py
@@ -123,14 +123,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-os.environ.get('SECRET_KEY')
+
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND'),
 EMAIL_HOST = os.environ.get('EMAIL_HOST'),
 EMAIL_FROM = os.environ.get('EMAIL_FROM'),
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER'),
 EMAIL_PORT = 587,
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD'),
+EMAIL_USE_TLS = True,
 
 LANGUAGE_CODE = 'en-us'
 
