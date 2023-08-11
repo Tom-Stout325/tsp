992c8aa5968438ebd51441d0f229c1158d114bea
Tom Stout <tom.stout325@gmail.com>
2023-08-10 20:44:35 -0400

    video embed

--- website/settings.py
+++ website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'photo.apps.PhotoConfig',
     'django_bootstrap_icons',
     'bootstrap5',
+    'embed_video',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',
             ],
         },
     },
